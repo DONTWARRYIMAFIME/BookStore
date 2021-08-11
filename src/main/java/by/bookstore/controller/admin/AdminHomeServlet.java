ac7ee8aec7d803e4495abe022c8b67b60f9884d3
Kostyukovich_V_A <demon4ik228228@gmail.com>
2021-08-12 00:25:57 +0300

    Implementing HomeServlet and AdminHomeServlet for redirecting to JSPs

---
+++ src/main/java/by/bookstore/controller/admin/AdminHomeServlet.java
@@ -0,0 +1,18 @@
+package by.bookstore.controller.admin;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AdminHomeServlet", value = "/admin")
+public class AdminHomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String homePage = "view/admin/index.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(homePage);
+        dispatcher.forward(request, response);
+    }
+
+}
---
+++ src/main/java/by/bookstore/controller/forntend/HomeServlet.java
@@ -0,0 +1,18 @@
+package by.bookstore.controller.forntend;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", value = "")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String homePage = "view/frontend/index.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(homePage);
+        dispatcher.forward(request, response);
+    }
+
+}
